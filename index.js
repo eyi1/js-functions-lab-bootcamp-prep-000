132141559f183e589303a5874fecb02479a03cbe
eyi1 <ej7729@gmail.com>
2018-01-31 23:27:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // write your code below!
+function happyHolidays(){
+  return "Happy holidays!"
+}
+
+happyHolidays();
+
+
+
+function happyHolidays(name){
+  return `Happy holidays, ${name}!`
+ }
+
+ happyHolidays("Grace");
